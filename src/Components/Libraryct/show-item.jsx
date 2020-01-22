7d9ac5ecf5becdec1ec590a3e9eebe2a7bac780b
Molly Madden <molly.annette.madden@gmail.com>
2020-01-22 11:04:38 +1100

    started show page - now rendering dynamic title

--- src/Components/Libraryct/show-item.jsx
+++ src/Components/Libraryct/show-item.jsx
@@ -2,16 +2,35 @@ import React from 'react';
 import Header from '../Shared/Header';
 import Title from '../Shared/Title';
 
+
 class Show extends React.Component {
+    state = {
+        item: {}
+    }
+
+
+    async componentDidMount() {
+        const response = await fetch("https://vast-headland-25884.herokuapp.com/items/" + this.props.match.params.id)
+        const data = await response.json()
+        this.setState({
+            item: data
+        })
+    }
+
     render() {
+        console.log("https://vast-headland-25884.herokuapp.com/items/" + this.props.match.params.id)
+        const { item } = this.state;
+
         return (
-            <div className="Main">
+            <div >
                 <Header />
-                <Title title={items.itemName} />
+                <Title title={item.itemName} />
             </div>
         )
     }
 }
 
 
+
 export default Show;
+
