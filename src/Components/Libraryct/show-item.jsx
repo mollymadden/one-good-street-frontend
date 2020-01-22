a6884335115c6410c91fd6af3b7f6c40e09ad937
Molly Madden <molly.annette.madden@gmail.com>
2020-01-22 11:38:17 +1100

    added ternary for delivery

--- src/Components/Libraryct/show-item.jsx
+++ src/Components/Libraryct/show-item.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../Shared/Header';
 import Title from '../Shared/Title';
+import './show-item.css'
 
 
 class Show extends React.Component {
@@ -21,10 +22,43 @@ class Show extends React.Component {
         console.log("https://vast-headland-25884.herokuapp.com/items/" + this.props.match.params.id)
         const { item } = this.state;
 
+        // let name = "";
+        // let phone = "";
+        // let email = "";
+
+        // if ({ item.privacy } == "true") {
+        //     name = { item.firstName },
+        //         phone = { item.phone },
+        //         email = { item.email }
+        // }
+        // else {
+        //     name = "Matiu Bush",
+        //         phone = "0455 555 555",
+        //         email = "info@onegoodstreet.com.au"
+        // };
+
+
         return (
             <div >
                 <Header />
                 <Title title={item.itemName} />
+
+
+                <div className="mainshowdiv">
+                    <img src={item.image} />
+                    <div className="showtext">
+                        <p>{item.description}</p>
+                        <p>📍 {item.postcode}</p>
+
+
+
+                        {/* <p>Please enquire with {name} on {phone} or at {email}</p> */}
+                        <p>{item.delivery ? "✅ Delivery available" : "❌ Delivery unavailable"}</p>
+
+                    </div>
+
+                </div>
+
             </div>
         )
     }
