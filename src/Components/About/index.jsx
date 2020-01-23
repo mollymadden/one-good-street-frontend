be82e991699450fabd3715c5e31b29c3808f0da7
Molly Madden <molly.annette.madden@gmail.com>
2020-01-23 21:36:03 +1100

    fixed css

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -18,34 +18,37 @@ class About extends React.Component {
                         <p>One Good Street seeks to strengthen all the links in the chain that keep older residents independent in their own homes for as long as possible. Those links in the chain include family members, friends and carers, community nurses, GP’s and home support services.</p>
                     </div>
 
-                <div className="blurb-border-title">How It Works</div>
-                <div className="blurb-border">
-                    <p>Streets are recognised as being Good Streets by signing up to the website and joining in the One Good Street activities. A sense of connectedness to local communities, and of belonging to others, is an important antidote to loneliness for many older people. Activities are practical, low commitment and happen regularly in your local neighbourhoods.</p>
-                </div>
-                <br />
-                <div className="blurb-border">
-                <p><em>"In our neighbourhoods, a lonely older person’s day, week and month is determined by the many actions taken daily by neighbours, actions that can be influenced on what these neighbours believe about a lonely older person’s future and their own role in improving it."</em></p>
-                </div>
-                <div className="blurb-border-title">The Journey So Far</div>
-                <div className="blurb-border">
-                    <p>1024 Members. 4 Active Suburbs. 3 Projects Running</p>
-                </div>
+                    <div className="blurb-border-title">How It Works</div>
+                    <div className="blurb-border">
+                        <p>Streets are recognised as being Good Streets by signing up to the website and joining in the One Good Street activities. A sense of connectedness to local communities, and of belonging to others, is an important antidote to loneliness for many older people. Activities are practical, low commitment and happen regularly in your local neighbourhoods.</p>
+                    </div>
+                    <br />
+                    <div className="blurb-border">
+                        <p><em>"In our neighbourhoods, a lonely older person’s day, week and month is determined by the many actions taken daily by neighbours, actions that can be influenced on what these neighbours believe about a lonely older person’s future and their own role in improving it."</em></p>
+                    </div>
+                    <div className="blurb-border-title">The Journey So Far</div>
+                    <div className="blurb-border">
+                        <p>1024 Members. 4 Active Suburbs. 3 Projects Running</p>
+                    </div>
 
 
-                <div className="blurb-border-title">GOOD STREETS TO AGE IN</div>
+                    <div className="blurb-border-title">GOOD STREETS TO AGE IN</div>
 
-                <div className="blurb-border">
+                    <div className="blurb-border">
 
 
-                <div className="float-left-p">
-                    <img src={hands} />
+                        <div className="float-left-p">
+                            <img src={hands} />
+                            <div className="p">
+                                <p>One Good Street seeks to create opportunities for residents to make a positive difference in the lives of their senior neighbours. We work with initiatives like casserole club, cycling without ageing, community houses and local councils to promote activities that reduce loneliness and isolation. &nbsp;</p>
 
-                    <p>One Good Street seeks to create opportunities for residents to make a positive difference in the lives of their senior neighbours. We work with initiatives like casserole club, cycling without ageing, community houses and local councils to promote activities that reduce loneliness and isolation. &nbsp;
+                                <p>Activities are practical, low commitment and you can join at short notice with the aim of promoting neighbourhood participation around the older person. Involvement in these activities makes your street a great street to age in.</p>
+                                <button type="button" className="btn btn-info" onClick={() => { this.props.history.push("/get-involved") }} type="button">Register Your Street</button>
 
-                    Activities are practical, low commitment and you can join at short notice with the aim of promoting neighbourhood participation around the older person. Involvement in these activities makes your street a great street to age in.</p>
-                </div>
-                
-                </div>
+                            </div>
+                        </div>
+
+                    </div>
 
 
 
